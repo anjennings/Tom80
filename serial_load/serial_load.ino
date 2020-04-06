0424fd92deafb559613d0783b52a2e0a4d01da7c
Aidan Jennings <anjennings@wisc.edu>
2020-04-06 13:25:23 -0500

    Update serial_load.ino

--- serial_load/serial_load.ino
+++ serial_load/serial_load.ino
@@ -1,35 +1,32 @@
 #define SER 2
 #define RCLK 3
 #define SRCLK 4
+#define WE 12
+#define ADDR_MASK_8 0x0FF
+#define ADDR_MASK_10 0x3FF
 
 #include <stdio.h>
 bool clk;
 char data;
 
 //TODO : make a header file
-void printStatus(int val){
+void printStatus(int address, int val){
 
   char hex[4];
 
   sprintf(hex, "0x%x", val);
 
-  Serial.print("Recieved value");
+  Serial.print("Write value");
+  Serial.print(" : ");
+  Serial.print(address);
   Serial.print(" : ");
   Serial.print(hex);
   Serial.print("\n\r");
   
 }
 
-void write8Bit(char d, bool LSB){
+void cycleCLK(){
 
-  for(int i = 0; i < 9; i++){
-
-    if(LSB){
-      digitalWrite(SER, (d>>i)&1);
-    }else{
-      digitalWrite(SER, (d<<i)&0x80);
-    }
-    
     delay(1);
     digitalWrite(RCLK, 1);
     delay(1);
@@ -38,10 +35,46 @@ void write8Bit(char d, bool LSB){
     digitalWrite(RCLK, 0);
     delay(1);
     digitalWrite(SRCLK, 0);
+  
+}
+
+void write8Bit(char d, bool LSB){
+
+  for(int i = 0; i < 8; i++){
+
+    if(LSB){
+      digitalWrite(SER, (d>>i)&1);
+    }else{
+      digitalWrite(SER, (d<<i)&0x80);
+    }
+
+    cycleCLK();
     
   }
 
   delay(1);
+  
+}
+
+void writeINST(int address, char data, bool LSB, int addr_mask){
+
+  int addr_valid = address & addr_mask;
+  char addr_l = (char)(addr_valid & 0xFF);
+  char addr_h = (char)((addr_valid & 0xF00)>>8);
+  
+  //clear all bits
+  write8Bit(0x00, LSB);
+  write8Bit(0x00, LSB);
+  write8Bit(0x00, LSB);
+  //write
+  write8Bit(data, LSB);//left
+  write8Bit(addr_l, true);//midle
+  write8Bit(addr_h, LSB);//right
+  cycleCLK();
+
+  digitalWrite(WE, false);
+  delayMicroseconds(150);
+  digitalWrite(WE, true);
   
 }
 
@@ -50,27 +83,24 @@ void setup() {
   pinMode(SER, OUTPUT);
   pinMode(RCLK, OUTPUT);
   pinMode(SRCLK, OUTPUT);
+  pinMode(WE,OUTPUT);
 
-  clk = 0;
-  data = 0x00;
-
+  
+  digitalWrite(WE, true);
   digitalWrite(LED_BUILTIN, false);
   Serial.begin(9600);
-  
-  //clear all bits
-  write8Bit(data, false);
-  
+
+  for(int i = 0; i <= 0xFF; i++){
+
+    writeINST(i, 0x00, false, ADDR_MASK_8);
+    printStatus(i, 0x00);
+    
+  }
+  digitalWrite(LED_BUILTIN, true);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  if(Serial.available() > 0){
-      data = Serial.read();
-      printStatus(data);
-      write8Bit(data, false);
-  }
-
   
 
 }
