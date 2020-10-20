c271d472e3e3cec1f5bb0f7899dffe67dcb7495d
Aidan Jennings <anjennings@wisc.edu>
2020-10-19 20:10:29 -0500

    reworked the tokenizer loop

--- src/ash.z80.asm
+++ src/ash.z80.asm
@@ -56,50 +56,51 @@ SYM_HELP equ "?"
 ;/////////////////
 org 0000h
 BOOT:
-;Wait a few clocks
-NOP
-NOP
-NOP
-NOP
-;Set up Stack
-LD H, STACK_H
-LD L, STACK_L
-LD SP, HL
-;Set up UART
-CALL UART_INIT
-;Disable interrupt
-IM 1
-DI
-;Boot Sequence Complete
-JP MAIN
+    ;Wait a few clocks
+    NOP
+    NOP
+    NOP
+    NOP
+    ;Set up Stack
+    LD H, STACK_H
+    LD L, STACK_L
+    LD SP, HL
+    ;Set up UART
+    CALL UART_INIT
+    ;Disable interrupt
+    IM 1
+    DI
+    ;Boot Sequence Complete
+    JP MAIN
 
-;//////////////////
-;Interrupt Routine
-;//////////////////
-org 0038h
-NOP
-NOP
-RETI
+    ;//////////////////
+    ;Interrupt Routine
+    ;//////////////////
+    org 0038h
+    NOP
+    NOP
+    RETI
 
 MAIN:
-;Print Boot Screen
-LD BC, BOOT_MSG
-CALL WRITE_STR
+    ;Print Boot Screen
+    LD BC, BOOT_MSG
+    CALL WRITE_STR
 
-;Print Ready Message
-LD BC, READY_MSG
-CALL WRITE_STR
+    ;Print Ready Message
+    LD BC, READY_MSG
+    CALL WRITE_STR
 
-;Set Terminal buffer to 0 
-LD BC, TERM_BUF
-LD A, 0
-LD (BC), A
+    ;Set Terminal buffer to 0 
+    LD BC, TERM_BUF
+    LD A, 0
+    LD (BC), A
 
 MAIN_LOOP:
-CALL GETCH
-JP MAIN_LOOP
+    ;CALL GETCH
+    CALL EVALUATE_STMT
+    JP MAIN_LOOP
 
-HALT
+    HALT
 
 ;//////////////////////
 ;//////Functions///////
@@ -109,65 +110,65 @@ HALT
 ;Set up UART
 ;//////////////////////////////////////
 UART_INIT:
-PUSH AF
-;Set DLAB=0, just in case
-IN A, UART_LCR
-AND 7Fh
-OUT UART_LCR, A
-;Disable All Interrupts
-LD A, 0 
-OUT UART_IER, A
-;FIFO Enable
-LD A, 1
-OUT UART_IFR, A
-;Line Control
-LD A, 03h         ;8 Bit word, 1 stop, no parity
-OUT UART_LCR, A
-;Set OUT pins Low (as an indicator)
-LD A, 0Ch      
-OUT UART_MCR, A
-;Set DLAB=1
-IN A, UART_LCR
-OR 80h
-OUT UART_LCR, A
-;Divide Clock by 6 for 19200 baud (Assuming 1.7MHz clock)
-LD A, 6
-OUT UART_DHR, A
-LD A, 0
-OUT UART_IER, A
-;Return
-POP AF
-RET
+    PUSH AF
+    ;Set DLAB=0, just in case
+    IN A, UART_LCR
+    AND 7Fh
+    OUT UART_LCR, A
+    ;Disable All Interrupts
+    LD A, 0 
+    OUT UART_IER, A
+    ;FIFO Enable
+    LD A, 1
+    OUT UART_IFR, A
+    ;Line Control
+    LD A, 03h         ;8 Bit word, 1 stop, no parity
+    OUT UART_LCR, A
+    ;Set OUT pins Low (as an indicator)
+    LD A, 0Ch      
+    OUT UART_MCR, A
+    ;Set DLAB=1
+    IN A, UART_LCR
+    OR 80h
+    OUT UART_LCR, A
+    ;Divide Clock by 6 for 19200 baud (Assuming 1.7MHz clock)
+    LD A, 6
+    OUT UART_DHR, A
+    LD A, 0
+    OUT UART_IER, A
+    ;Return
+    POP AF
+    RET
 
 
 ;//////////////////////////////////////
 ;Get a character from the FIFO, add to write buffer and echo to screen
 ;//////////////////////////////////////
 GETCH:
-PUSH AF
-PUSH BC
-;Set DLAB 0
-IN A, UART_LCR
-AND 7Fh
-OUT UART_LCR, A
-GETCH_LOOP:
-;Read Line Status Reg
-IN A, UART_LSR
-;Only care about bit 0
-AND 1
-;If bit 0 is a 1 then FIFO has new data
-CP 1
-;Jump to end if bit 0 was a 0
-JP NZ, GETCH_END
-;Get next char from data holding register
-IN A, UART_DHR
-CALL WRITE_BUFFER
-JP GETCH_LOOP
+    PUSH AF
+    PUSH BC
+    ;Set DLAB 0
+    IN A, UART_LCR
+    AND 7Fh
+    OUT UART_LCR, A
+    GETCH_LOOP:
+    ;Read Line Status Reg
+    IN A, UART_LSR
+    ;Only care about bit 0
+    AND 1
+    ;If bit 0 is a 1 then FIFO has new data
+    CP 1
+    ;Jump to end if bit 0 was a 0
+    JP NZ, GETCH_END
+    ;Get next char from data holding register
+    IN A, UART_DHR
+    CALL WRITE_BUFFER
+    JP GETCH_LOOP
 
-GETCH_END:
-POP BC
-POP AF
-RET
+    GETCH_END:
+    POP BC
+    POP AF
+    RET
 
 
 ;///////////////////////////////////////
@@ -175,50 +176,50 @@ RET
 ;expects A to be the character
 ;//////////////////////////////////////
 WRITE_BUFFER:
-PUSH AF
-PUSH BC
-PUSH DE
+    PUSH AF
+    PUSH BC
+    PUSH DE
 
-;Save character in D
-LD D, A
-;Load address of terminal buffer
-LD BC, TERM_BUF
-;Get size of terminal buffer
-LD A, (BC)
-;Add 1
-INC A
-;Write new length to start of buffer
-LD (BC), A
-;Add A and C for new offset (C should be 0 but just in case)
-ADD A, C
-;Put A into C
-LD C, A
-;Put char back into A
-LD A, D
-;Write to buffer
-LD (BC), A
+    ;Save character in D
+    LD D, A
+    ;Load address of terminal buffer
+    LD BC, TERM_BUF
+    ;Get size of terminal buffer
+    LD A, (BC)
+    ;Add 1
+    INC A
+    ;Write new length to start of buffer
+    LD (BC), A
+    ;Add A and C for new offset (C should be 0 but just in case)
+    ADD A, C
+    ;Put A into C
+    LD C, A
+    ;Put char back into A
+    LD A, D
+    ;Write to buffer
+    LD (BC), A
 
-CALL PRINTCH
+    CALL PRINTCH
 
-POP DE
-POP BC
-POP AF
-RET
+    POP DE
+    POP BC
+    POP AF
+    RET
 
 ;/////////////////////////////////////////
 ;Assumes that A is the charactar to write
 ;/////////////////////////////////////////
 PRINTCH:
-PUSH AF
-;Set DLAB 0
-IN A, UART_LCR
-AND 7Fh
-OUT UART_LCR, A
-;TODO: read transmit register status? Page 22
-;Write Char to UART
-OUT UART_DHR, A
-POP AF
-RET
+    PUSH AF
+    ;Set DLAB 0
+    IN A, UART_LCR
+    AND 7Fh
+    OUT UART_LCR, A
+    ;TODO: read transmit register status? Page 22
+    ;Write Char to UART
+    OUT UART_DHR, A
+    POP AF
+    RET
 
 
 ;////////////////////////////////////////
@@ -228,142 +229,189 @@ RET
 ;This is really more of a placeholder until I get more info about the UART
 ;////////////////////////////////////////
 WRITE_STR:
-DI
-PUSH AF
-PUSH BC
-;Set DLAB 0
-IN A, UART_LCR
-AND 7Fh
-OUT UART_LCR, A
-WRITE_START:
-LD A, (BC)
-CP 0
-JP Z, WRITE_CLOSE
-;OUT UART_DHR, A
-CALL PRINTCH
-INC BC
-JP WRITE_START
-WRITE_CLOSE:
-POP BC
-POP AF
-EI
-ret
+    DI
+    PUSH AF
+    PUSH BC
+    ;Set DLAB 0
+    IN A, UART_LCR
+    AND 7Fh
+    OUT UART_LCR, A
+    WRITE_START:
+    LD A, (BC)
+    CP 0
+    JP Z, WRITE_CLOSE
+    ;OUT UART_DHR, A
+    CALL PRINTCH
+    INC BC
+    JP WRITE_START
+    WRITE_CLOSE:
+    POP BC
+    POP AF
+    EI
+    ret
 
-;////////////////////////////////////////
-;Parse user input for commands and execute when valid
-;Modifies ~0x8100
-;0x8100 is the size of the parser buffer
-;This should SOMEWHAT resemble a LL(1) parser
-PARSE_BUF equ 0x8100
-;////////////////////////////////////////
+;Main function to tokenize, parse, and execute user entered expressions
+;Assume AF has return values
+EVALUATE_STMT:
+    PUSH AF
+
+    CALL TOKENIZE_BUFFER
+    CALL PARSE_BUFFER
+
+    POP AF
+    RET
+
+
+;There are 5 types of symbols - LIT, @, :, <, ?
+;Current and next token for parser
+;Returns status in register A
+;0x00 - Good
+;0xFF - Bad
+PARSE_CUR equ 0x8100
+PARSE_NEXT equ 0x8102
+;Buffer for tokens, first byte is size of buffer
+TOKEN_BUF equ 0x8110
+
+TOKEN_EF equ 0   ;End of buffer
+TOKEN_LT equ 1   ;ABCDEF0123456789 size 3
+TOKEN_EX equ 2   ;@ size 1
+TOKEN_RD equ 3   ;: size 1
+TOKEN_WR equ 4   ;< size 1
+TOKEN_HE equ 5   ;? size 1
+
+TOKENIZE_BUFFER:
+    PUSH BC
+    PUSH DE
+
+    ;Clear parse buffer
+    LD DE, TOKEN_BUF
+    LD A, 0
+    LD (DE), A
+
+    ;Get start of terminal buffer
+    LD DE, TERM_BUF
+
+    TOKENIZE_BUFFER_LOOP:
+        ;Get next character
+        INC DE
+        LD A, (DE)
+        ;Save character
+        LD B, A
+
+        ;/////////////////////
+        ;Check if return
+        ;/////////////////////
+        CP NEWLINE
+        JP Z, TOKENIZE_BUFFER_RETURN
+
+        ;/////////////////////
+        ;Check if a number
+        ;/////////////////////
+        SUB 0x30
+        CP 0xA
+        CALL C, TOKENIZE_NUMBERS
+        ;Return to start of loop if return is FF
+        CP 0xFF
+        JP Z, TOKENIZE_BUFFER_LOOP
+        ;Return original character
+        LD A, B
+
+        ;/////////////////////
+        ;Check if a hex character
+        ;/////////////////////
+        SUB 0x41
+        CP 0x6
+        CALL C, TOKENIZE_CHAR
+        ;Return to start of loop if return is FF
+        CP 0xFF
+        JP Z, TOKENIZE_BUFFER_LOOP
+        ;Return original character
+        LD A, B
+
+        ;/////////////////////
+        ;Check if a ?
+        ;/////////////////////
+        CP SYM_HELP
+        CALL Z, TOKENIZE_HELP
+        ;Return to start of loop if return is FF
+        CP 0xFF
+        JP Z, TOKENIZE_BUFFER_LOOP
+        ;Return original character
+        LD A, B
+
+        ;/////////////////////
+        ;Check if a :
+        ;/////////////////////
+        CP SYM_READ
+        CALL Z, TOKENIZE_READ
+        ;Return to start of loop if return is FF
+        CP 0xFF
+        JP Z, TOKENIZE_BUFFER_LOOP
+        ;Return original character
+        LD A, B
+
+        ;/////////////////////
+        ;Check if a <
+        ;/////////////////////
+        CP SYM_WRITE
+        CALL Z, TOKENIZE_WRITE
+        ;Return to start of loop if return is FF
+        CP 0xFF
+        JP Z, TOKENIZE_BUFFER_LOOP
+        ;Return original character
+        LD A, B
+
+        ;/////////////////////
+        ;Check if a @
+        ;/////////////////////
+        CP SYM_EXE
+        CALL Z, TOKENIZE_EXE
+        ;Return to start of loop if return is FF
+        CP 0xFF
+        JP Z, TOKENIZE_BUFFER_LOOP
+
+
+        ;If the program gets to this point there is an error
+        LD A, 0xFF
+        ;Return
+
+
+    TOKENIZE_BUFFER_RETURN:
+    POP DE
+    POP BC
+    RET
+
+
+
+;Each of these should return 0xFF in A before exiting
 PARSE_BUFFER:
-PUSH AF
-PUSH DE
+    RET
 
-;Clear parse buffer
-LD DE, PARSE_BUF
-LD A, 0
-LD (DE), A
+;B should contain the character value, not A
+TOKENIZE_NUMBERS:
+    LD A, 0xFF
+    RET
 
-;Iterator
-LD B, 0
-PARSE_BUFFER_LOOP:
-;Get size of terminal buffer
-LD DE, TERM_BUF
-LD A, (DE) 
-;Check size of buffer
-CP B
-JP Z, PARSE_BUFFER_RETURN
-;Increment iterator
-INC B
-;Save Incrementor
-PUSH BC
-;Get address of next character
-LD A, B
-ADD A, E
-;Get next character
-LD A, (DE)
+TOKENIZE_CHAR:
+    LD A, 0xFF
+    RET
 
+TOKENIZE_READ:
+    LD A, 0xFF
+    RET
+    
+TOKENIZE_WRITE:
+    LD A, 0xFF
+    RET
+    
+TOKENIZE_EXE:
+    LD A, 0xFF
+    RET
 
-;TODO
-;Do some other stuff
-;A should still contain the charactar
-;
+TOKENIZE_HELP:
+    LD A, 0xFF
+    RET
 
-;Essentially just a big case statement
-;There must be a better way than this but im tired right now
-;Maybe subtracting 0x30 and checking if its less than 10?a
-
-;Load ff into E, each parse subroutine should set E to 0 if it succeeds
-;if E is still non 0 at the end then a syntax error has occured
-LD E, 0xFF
-
-;Check for numbers
-CP NUM0
-CALL Z, PARSE_NUMBER
-CP NUM1
-CALL Z, PARSE_NUMBER
-CP NUM2
-CALL Z, PARSE_NUMBER
-CP NUM3
-CALL Z, PARSE_NUMBER
-CP NUM4
-CALL Z, PARSE_NUMBER
-CP NUM5
-CALL Z, PARSE_NUMBER
-CP NUM6
-CALL Z, PARSE_NUMBER
-CP NUM7
-CALL Z, PARSE_NUMBER
-CP NUM8
-CALL Z, PARSE_NUMBER
-CP NUM9
-CALL Z, PARSE_NUMBER
-
-CP SYM_READ
-CALL Z, PARSE_INST
-CP SYM_WRITE
-CALL Z, PARSE_INST
-CP SYM_EXE
-CALL Z, PARSE_INST
-CP SYM_HELP
-CALL Z, PARSE_INST
-
-;Check that E is no longer non 0
-LD A, E
-CP 0
-JP NZ, PARSE_BUFFER_SYNTAX
-
-;Return Incrementor to BC
-POP BC
-
-;This should probably be conditional
-JP PARSE_BUFFER_LOOP
-
-PARSE_BUFFER_SYNTAX:
-;Welcome to the shadow realm
-LD BC, SYNTAX_ERROR
-CALL WRITE_STR
-;Exit
-PARSE_BUFFER_RETURN:
-POP DE
-POP AF
-RET
-
-
-;//////////////////////
-;Parse Numbers
-;TODO
-;//////////////////////
-PARSE_NUMBER:
-RET
-
-;//////////////////////
-;Parse Instructions
-;TODO
-;//////////////////////
-PARSE_INST:
-RET
 
 ;//////////////////////
 ;/////////DATA/////////
@@ -381,3 +429,6 @@ SYNTAX_ERROR:
 
 CURSOR:
 .db RETURN, ">>>:", EOF
+
+org 8001h
+.db "A0:7F", NEWLINE
