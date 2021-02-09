fcbc14b07e93ff1caaa7c1dda4a25a72a126144c
Aidan Jennings <anjennings@wisc.edu>
2021-02-08 18:48:38 -0600

    after much fine tuning...

--- src/zdk projects/uart/New folder/mand.c
+++ src/zdk projects/uart/New folder/mand.c
@@ -1,40 +1,14 @@
 #include "mand.h"
 	
 int main(){
-	//printf("hello world!\n");
 	mandlebrot();
 	return 0;
 }
 
 void mandlebrot(){
-	
-	/*
-	for each pixel (Px, Py) on the screen do
-    x0 := scaled x coordinate of pixel (scaled to lie in the Mandelbrot X scale (-2.5, 1))
-    y0 := scaled y coordinate of pixel (scaled to lie in the Mandelbrot Y scale (-1, 1))
-    x := 0.0
-    y := 0.0
-    iteration := 0
-    max_iteration := 1000
-    while (x*x + y*y ≤ 2*2 AND iteration < max_iteration) do
-        xtemp := x*x - y*y + x0
-        y := 2*x*y + y0
-        x := xtemp
-        iteration := iteration + 1
- 
-    color := palette[iteration]
-    plot(Px, Py, color)
-	*/
-	
-	
-	
-	
 	for(int i = 0; i < HEIGHT; i++){
-		//printf("y is %d, y_step is %d, y_range is %d\n", y_MAX - (i*y_STEP), y_STEP, y_RANGE);
 		for(int j = 0; j < WIDTH; j++){
-			//printf("\tx is %d, x_step is %d, x_range is %d\n", x_MIN + (j*x_STEP), x_STEP, x_RANGE);
 			printf("%c", step(j, i));
-			
 		}
 		printf("\n");
 	}
@@ -49,11 +23,11 @@ char step(int px, int py){
 	int iteration = 0;
 	int xtemp = 0;
 	
-	while((((x*x)/(SCALE*SCALE)) + ((y*y)/(SCALE*SCALE)) <= UNKNOWN_CONSTANT) & (iteration < ITERATION_MAXIMUM)){
-		xtemp = ((x*x)/(SCALE)) - ((y*y)/(SCALE)) + x0;
+	while((((x*x)) + ((y*y)) <= UNKNOWN_CONSTANT) & (iteration < ITERATION_MAXIMUM)){
+		xtemp = ((x*x)/(SCALE)) - ((y*y)/(SCALE)) + (x0);
 		y = ((2*x*y)/(SCALE)) + y0;
 		x = xtemp;
 		iteration++;
 	}
-	return 	iteration > (ITERATION_MAXIMUM/50) ? CHAR_ON : CHAR_OFF;
+	return 	iteration == (ITERATION_MAXIMUM) ? CHAR_ON : CHAR_OFF;
 }
--- src/zdk projects/uart/New folder/mand.h
+++ src/zdk projects/uart/New folder/mand.h
@@ -5,31 +5,32 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-//#define int int16_t
+#define int int16_t
 
-#define WIDTH 75
-#define HEIGHT 20
+#define WIDTH 100
+#define HEIGHT 28
 
-#define SCALE 100
+#define SCALE 10000
 
 #define x_MAX 1
 #define x_MIN -2.5
 #define y_MAX 1
 #define y_MIN -1
 
+#define ITERATION_MAXIMUM 100
+
 #define x_SCALE_MAX (x_MAX * SCALE)
 #define x_SCALE_MIN (x_MIN * SCALE)
 #define y_SCALE_MAX (y_MAX * SCALE)
 #define y_SCALE_MIN (y_MIN * SCALE)
 
-#define x_RANGE (float)(x_SCALE_MAX-x_SCALE_MIN)
-#define y_RANGE (float)(y_SCALE_MAX-y_SCALE_MIN)
+#define x_RANGE (x_SCALE_MAX-x_SCALE_MIN)
+#define y_RANGE (y_SCALE_MAX-y_SCALE_MIN)
 
 #define x_STEP (x_RANGE/WIDTH)
 #define y_STEP (y_RANGE/HEIGHT)
 
-#define ITERATION_MAXIMUM 1000
-#define UNKNOWN_CONSTANT 4
+#define UNKNOWN_CONSTANT (4 * SCALE * SCALE)
 
 #define CHAR_ON '.'
 #define CHAR_OFF ' '
