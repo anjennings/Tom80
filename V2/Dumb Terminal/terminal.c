7eccd66e0eecbc9c880973adcfbf4c7ee27104df
Aidan Jennings <anjennings@wisc.edu>
2021-05-23 17:40:15 -0500

    ASH VGA has drivers to print text to screen

--- V2/Dumb Terminal/terminal.c
+++ V2/Dumb Terminal/terminal.c
@@ -2,21 +2,39 @@
 #include "vgatext.h"
 #include "PIO.h"
 
+volatile char vga_o;
+
 vgatext *vga;                                 // VGA identifier
 
-int main(void)                                // Main function
-{
+void VGA_Proc() {
+  
   vga = vgatext_open(16);                      // Open VGA port with P0 base
-    
-  dprint(vga,"Hello VGA.\n");                 // Message + newline
-  dprint(vga,"Hello again!\n");               // Another message
-  vgatext_setColors(5);                       // Set color palette
+  vgatext_setColors(0);                        // Set color palette
   
   char d = 0;
   while(1) {
-    d = (char)readPIO();
-    vgatext_putchar(vga, (0x7F & d));
-    printf("I got a %c \n", (0x7F & d));
+    while(vga_o == 0) {
+      // spin wait 
+    }  
+    d = vga_o;
+    vga_o = 0;    
+    //d = (char)readPIO();
+    if((d >= 32) && (d <= 126)) {
+      vgatext_putchar(vga, (0x7F & d));
+      printf("I got a %c \n", (0x7F & d));
+    } else if (d == '\n') {
+      vgatext_putchar(vga, '\n');
+    }            
   }    
+}  
 
+
+int main(void)                                // Main function
+{
+  cog_run(VGA_Proc, 1024);
+  
+  while(1) {
+    if (vga_o == 0)
+      vga_o = readPIO();
+  }
 }
