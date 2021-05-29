ff7cf8653491ad1164e86b017e9ef18331887e1b
Aidan Jennings <anjennings@wisc.edu>
2021-05-29 16:46:19 -0500

    Added more disk routines

--- V2/Dumb Terminal/Disk.c
+++ V2/Dumb Terminal/Disk.c
@@ -4,21 +4,95 @@
 SimpleDisk DriveA;
 SimpleDisk DriveB;
 
-void createDiskImage(SimpleDisk * Drive) {
-  //Create a dummy sector filled with 0's
-  char * dummySector;
-  dummySector = (char *) calloc((Drive->SectorSize), sizeof(uint8_t));
-    
-  // Create new File  
-  Drive->image = fopen("A.img", "wb");
-    
-  //Fill disk image with 0'ed out sectors
-  for(int i = 0; i < diskSectors((*Drive)); i++){
-    fwrite(dummySector, sizeof(uint8_t), Drive->SectorSize, Drive->image);
-  }
-  fclose(Drive->image); 
+/**
+ *  Called after write init command recieved
+ *  Transfer data from PIO into buffer and write buffer to file
+ **/
+void handleWrite(SimpleDisk * Drive) {
+  for (int i = 0; i < Drive->SectorSize; i++) {
+    Drive->Buffer[i] = readPIO(); 
+  } 
+  writeSector(Drive);
+}
+
+/**
+ *  Called after read init command recieved
+ *  Read data from file into buffer and transfer buffer to PIO
+ **/
+void handleRead(SimpleDisk * Drive) {
+  readSector(Drive);
+  for (int i = 0; i < Drive->SectorSize; i++) {
+    writePIO(Drive->Buffer[i]);
+  } 
 }  
 
+/**
+ *  Write buffer back into file
+ *  Does NOT recieve data to PIO
+ **/
+void writeSector(SimpleDisk * Drive) {
+  long int tOffset = Drive->CurrentTrack * Drive->Sectors * Drive->SectorSize;
+  long int sOffset = Drive->CurrentSector * Drive->SectorSize;
+  long int offset = tOffset + sOffset;
+  
+  fseek(Drive->image, offset, SEEK_SET);
+  fwrite(Drive->Buffer, sizeof(uint8_t), Drive->SectorSize, Drive->image);
+}
+
+/**
+ *  Read sector of file into buffer
+ *  Does NOT send data to PIO
+ **/
+void readSector(SimpleDisk * Drive) {
+  long int tOffset = Drive->CurrentTrack * Drive->Sectors * Drive->SectorSize;
+  long int sOffset = Drive->CurrentSector * Drive->SectorSize;
+  long int offset = tOffset + sOffset;
+  
+  fseek(Drive->image, offset, SEEK_SET);
+  fread(Drive->Buffer, sizeof(uint8_t), Drive->SectorSize, Drive->image);
+}  
+
+// Set current track on drive
+int setTrack(SimpleDisk * Drive, uint8_t track) {
+  
+  if ((track < 0) || (track >= Drive->Tracks))
+    return -1;
+  Drive->CurrentTrack = track;  
+  return 0;
+}  
+
+// Set current sector on drive
+int setSector(SimpleDisk * Drive, uint8_t sector) {
+  
+  if ((sector < 0) || (sector >= Drive->Sectors))
+    return -1; 
+  Drive->CurrentSector = sector;  
+  return 0;
+} 
+
+// Create disk image of appropriate size if none exists for this drive
+void createDiskImage(SimpleDisk * Drive) {
+    
+  // Create filename from Drive letter
+  char src[16];
+  char dst[] = ".IMG";
+  src[0] = Drive->DriveLetter;
+  src[1] = '\0';
+  strcat(src, dst);
+  
+  // Create new File  
+  Drive->image = fopen(src, "wb");
+    
+  //Fill disk image with unused sectors
+  for(int i = 0; i < diskSectors((*Drive)); i++){
+    fwrite(Drive->Buffer, sizeof(uint8_t), Drive->SectorSize, Drive->image);
+  } 
+}  
+
+
+/**
+ *  Initilize the drive for reading and writing to disk image
+ **/
 void initDriveA() {
   DriveA.Sectors = 26;
   DriveA.Tracks = 77;
@@ -27,16 +101,18 @@ void initDriveA() {
   DriveA.DriveLetter = 'A';
   DriveA.CurrentSector = 0;
   DriveA.CurrentTrack = 0;
-  DriveA.image = fopen("A.img", "rb+");
+  DriveA.Buffer = calloc(DriveA.SectorSize, sizeof(uint8_t));
+  DriveA.image = fopen("A.img", "rb+");   //Open simply to see if the file exists
   
-  //If no file exists, create a new one (might take a while based on how large the image should be)
+  //If no file exists, create a new one (might take a minute or so based on how large the image should be)
   if(DriveA.image == NULL) {
     fclose(DriveA.image);
     createDiskImage(&DriveA);  
   }
-  //Image should now exist, new or old
-  
-  
+  fclose(DriveA.image);
+  // Now file should be present and initilized
+  DriveA.image = fopen("A.img", "rb+");
+  //Drive should now be ready to read or write to
 }  
 
 // The largest potential disk CPM can support
@@ -49,19 +125,17 @@ void initDriveB() {
   DriveA.DriveLetter = 'B';
   DriveA.CurrentSector = 0;
   DriveA.CurrentTrack = 0;
+  // TODO
 }  
 
-void initDisk() {
+int initDisk() {
   
   printf("Init Disk!\n");
   if(sd_mount(SDDO, SDCLK, SDDI, SDCS)) {
     printf("SD Card Mount Failure!\n");
-    return;
+    return -1;
   }   
-  initDriveA();
-  
-  // Do stuff
-  
-  printf("Closing Disk!\n");
-  fclose(DriveA.image);
+  initDriveA();  
+  printf("Init Disk Complete!\n");
+  return 0;
 }  
--- V2/Dumb Terminal/Disk.h
+++ V2/Dumb Terminal/Disk.h
@@ -3,12 +3,15 @@
 
 #include "simpletools.h"
 #include "propeller.h"
+#include "PIO.h"
 
+// GPIO Pins
 #define SDCLK 27
 #define SDDI  26
 #define SDCS  25
 #define SDDO  24
 
+// Structs
 typedef struct SimpleDisk {
   uint8_t   Sectors;
   uint8_t   Tracks;
@@ -18,13 +21,23 @@ typedef struct SimpleDisk {
   uint8_t   CurrentSector;
   uint8_t   CurrentTrack;
   FILE*     image;
+  uint8_t*  Buffer;
 }SimpleDisk;  
 
+// Macros
 #define diskSectors(D) (D.Sectors * D.Tracks)
 #define diskCapacity(D) (diskSectors(D) * D.SectorSize)
 
+// Functions
 void initDriveA();
 void initDriveB();
-void initDisk();
+int initDisk();
+void createDiskImage(SimpleDisk * Drive);
+int setSector(SimpleDisk * Drive, uint8_t sector);
+int setTrack(SimpleDisk * Drive, uint8_t track);
+void readSector(SimpleDisk * Drive);
+void writeSector(SimpleDisk * Drive);
+void handleWrite(SimpleDisk * Drive);
+void handleRead(SimpleDisk * Drive)
 
 #endif // DISK_H_
