039f6c3dc47f8b1188c0f36773250d51cef11479
Aidan Jennings <anjennings@wisc.edu>
2021-06-03 08:59:34 -0500

    Added SD card check

--- V2/Propeller/SD Card Test/SD Card Test.c
+++ V2/Propeller/SD Card Test/SD Card Test.c
@@ -1,17 +1,31 @@
 /*
-  Blank Simple Project.c
-  http://learn.parallax.com/propeller-c-tutorials 
+  SD Minimal.side
+
+  Create test.txt, write characters in, read back out, display.
 */
-#include "simpletools.h"                      // Include simple tools
 
-int main()                                    // Main function
+#include "simpletools.h"                      // Include simpletools header    
+
+int DO = 24, CLK = 27, DI = 26, CS = 25;      // SD card pins on Propeller BOE
+
+int main(void)                                // main function
 {
-  // Add startup code here.
+  printf("boot\n");
+  if (sd_mount(DO, CLK, DI, CS)) {
+    printf("SD Mount Failed!\n");   
+  } else {                      // Mount SD card
 
+    FILE* fp = fopen("test.txt", "w");          // Open a file for writing
+    fwrite("Testing 123...\n", 1, 15, fp);      // Add contents to the file
+    fclose(fp);                                 // Close the file
  
-  while(1)
-  {
-    // Add main loop code here.
-    
-  }  
-}
+    char s[15];                                 // Buffer for characters
+    fp = fopen("test.txt", "r");                // Reopen file for reading
+    fread(s, 1, 15, fp);                        // Read 15 characters
+    fclose(fp);                                 // Close the file
+  
+    printf("First 15 chars in test.txt:\n");     // Display heading
+    printf("%s", s);                             // Display characters
+    printf("\n");                                // With a newline at the end
+  }
+}      
