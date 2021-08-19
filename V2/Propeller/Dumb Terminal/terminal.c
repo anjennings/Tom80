5836482dbfd1bfe50793b059e8284c21871da636
Aidan Jennings <anjennings@wisc.edu>
2021-08-18 21:14:38 -0500

    removed VGA core from propeller firmware

--- V2/Propeller/Dumb Terminal/terminal.c
+++ V2/Propeller/Dumb Terminal/terminal.c
@@ -1,45 +1,15 @@
 #include "propeller.h"
 #include "simpletools.h"
-#include "vgatext.h"
 #include "PIO.h"
 #include "Disk.h"
 
 #define DEBUG 0
-
-volatile char vga_o;
-
-vgatext *vga;
-
-/**
- *    Separate cog that handles VGA output
- **/
-void VGA_Proc() {
-  
-  vga = vgatext_open(16);                      // Open VGA port with P0 base
-  vgatext_setColors(0);                        // Set color palette
-  
-  char d = 0;
-  while(1) {
-    while(vga_o == 0) {
-      // spin wait 
-    }  
-    d = vga_o;
-    vga_o = 0;    
-    if((d >= 32) && (d <= 126)) {
-      vgatext_putchar(vga, (0x7F & d));
-    } else if (d == '\n') {
-      vgatext_putchar(vga, '\n');
-    }            
-  }    
-}  
 
 /**
  *    Given command, jump to appropriate subroutine
  **/
 
 int processCommand(uint8_t data) {
-  
-  uint8_t foo = 0;
   
   switch(data) {
         
@@ -61,7 +31,6 @@ int processCommand(uint8_t data) {
       break;
           
     case 0x93 :                         // Prepare to read (Load sector into buffer)
-      //readSector(currentDrive);
       handleRead(currentDrive);
       return 0;
       break;
@@ -95,20 +64,9 @@ int processCommand(uint8_t data) {
 
 int main(void)                          // Main function
 {
-  cog_run(VGA_Proc, 1024);
-  uint8_t data;
   initDisk();
   
   while(1) {
-    
-    data = readPIO();
-    
-    if (data < 0x80) {                  // if less than 0x80 its just an ascii char
-      while (vga_o != 0) {
-      }        
-      vga_o = data;
-    } else {
-      processCommand(data);     
-    }               
+    processCommand(readPIO());                    
   }
 }
